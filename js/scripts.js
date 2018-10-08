0aa8bf9f83a08390708f86284cf1dfd9e8fe56af
Addy <addyleeliu@gmail.com>
2018-10-08 16:21:46 -0400

    created custom slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,37 @@
 $(function() {
+
   console.log('jQ loaded all systems go');
+
 });
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  var topDots = document.getElementsByClassName("top-dot");
+  var bottomDots = document.getElementsByClassName("bottom-dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  topDots[slideIndex-1].className += " active";
+  bottomDots[slideIndex-1].className += " active";
+}
